beab974db7046ce5e21c1b3b15d53e40a49c3a0f
flynncowell <cowellflynn@gmail.com>
2019-10-18 09:53:01 +0100

    Update reciever.py

--- reciever.py
+++ reciever.py
@@ -1,14 +1,38 @@
-# Radio rock paper scissors
+# random Radio rock paper scissors
 from microbit import *
 import radio
 import random 
 
-choices = ["rock","paper","scisssors"]
+choices = ["rock","paper","scissors"]
 
 radio.on() #turn radio on 
 
 while True:
-    mychoice = radio.reciveve()
-        display.scroll(mychoice)
+    if button_a.was_pressed():
+       choice1= random.choice(choices)
+       choice2 = radio.recieve()
+       sleep(2000)
+       display.scroll(choice1)
+       radio.send(choice1)2
+       
+        sleep(5000)
+            if choice1 == rock and choice2 == scissors:
+                display.scroll("you won!")
+                elif choice1 == scissors and choice2 == rock:
+                    display.scroll("you lost!")
+                        elif choice1 == scissors and choice2 == paper:
+                            display.scroll("you won!")
+                             elif choice1 == paper and choice2 == scissors:
+                                 display.scroll("you lost!")
+                                    elif choice1 == paper and choice2 == rock:
+                                        display.scroll("you won!")
+                                            elif choice1 == rock and choice2 == paper:
+                                                display.scroll("you lost!")
+                                                    elif choice1 == rock and choice2 == rock:
+                                                        display.scroll("it's a draw!")
+                                                            elif choice1 == paper and choice2 == paper:
+                                                                display.scroll("it's a draw!")
+                                                                    elif choice1 == scissors and choice2 == scissors:    
+                                                                        display.scroll("it's a draw!")
         
         
